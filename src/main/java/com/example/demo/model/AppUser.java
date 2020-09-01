b1f0d6ff7a6d224f073ee420dd1b54ac8dcebf98
Gameweb <projektinz20@gmail.com>
2020-09-01 19:08:48 +0200

    heroku

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Getter
-@Document(collection = "user" )
+@Document(collection = "user")
 @Builder
 public class AppUser {
 
--- src/main/java/com/example/demo/model/Game.java
+++ src/main/java/com/example/demo/model/Game.java
@@ -1,42 +1,51 @@
 package com.example.demo.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.util.StringUtils;
 
+@Getter
+@Document(collection = "games")
+@Builder
 public class Game {
 
-    private String id;
+    @Id
+    public String id;
     private String name;
     private String category;
+    private String description;
+    private String platform;
 
-    public Game(String id, String name, String category) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-    }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        if (StringUtils.isEmpty(this.id)) {
-            this.id = id;
-        }
-    }
+//    public Game(String id, String name, String category) {
+//        this.id = id;
+//        this.name = name;
+//        this.category = category;
+//    }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setId(String id) {
+//        if (StringUtils.isEmpty(this.id)) {
+//            this.id = id;
+//        }
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(String category) {
+//        this.category = category;
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 }
