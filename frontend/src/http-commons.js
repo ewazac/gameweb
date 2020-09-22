a813ee8795c54265f1dcc283c0abc5fbf40c0554
Thrytios <dembiski321@gmail.com>
2020-09-22 09:34:56 +0200

    heroku

--- frontend/src/http-commons.js
+++ frontend/src/http-commons.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const AXIOS = axios.create({
   baseURL: `http://localhost:4200`,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8086/',
+    'Access-Control-Allow-Origin': 'https://gameweb21.herokuapp.com/',
     'Access-Control-Allow-Credentials': true,
   }
 })
