29ef9943f8da21a9eab30b90d042a94448224d7b
Gameweb <projektinz20@gmail.com>
2020-10-06 20:27:32 +0200

    zmiany w logowaniu

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,16 +2,14 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.AppUser;
 import com.example.demo.model.UserRepository;
-import com.example.demo.services.MongoUserDetailsService;
-import com.fasterxml.jackson.databind.ser.Serializers;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,7 +31,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void createUser(@Valid @RequestBody UserDTO userDTO) throws UserFoundException{
+    public void createUser(@Valid @RequestBody UserDTO userDTO) throws UserFoundException {
         AppUser appUser = userRepository.findUserByEmail(userDTO.getEmail());
         if(appUser != null) {
             throw new UserFoundException();
@@ -53,27 +51,36 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @RequestMapping(value ="/{id}/uploadAvatar", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value ="/uploadAvatar", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
-    public void addAvatar(@RequestParam("avatar") MultipartFile multipartFile, @PathVariable String id) throws IOException {
-        AppUser user = userRepository.findUserById(id);
+    public void addAvatar(@RequestParam("avatar") MultipartFile multipartFile) throws IOException {
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //getting user from session
+        userDetails.getUsername();
+        AppUser user = userRepository.findUserByEmail(userDetails.getUsername());
         if (user != null) {
             user.setAvatar(new Binary(BsonBinarySubType.BINARY, multipartFile.getBytes()));
             userRepository.save(user);
         }
     }
 
-    @GetMapping(value = "/{id}/getAvatar")
-    public Binary getAvatar(@PathVariable String id) {
-        AppUser appUser = userRepository.findUserById(id);
+    @GetMapping(value = "/getAvatar")
+    public Binary getAvatar() {
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //getting user from session
+        userDetails.getUsername();
+        AppUser appUser = userRepository.findUserByEmail(userDetails.getUsername());
         return appUser.getAvatar();
 
     }
 
-    @PutMapping(value = "/{id}/changePassword")
+    @PutMapping(value = "/changePassword")
     @ResponseStatus(value = HttpStatus.OK)
-    public void changePassword(@RequestParam("newpassword") String newPassword, @RequestParam("oldpassword") String oldPassword, @PathVariable String id) throws InvalidOldPasswordException {
-        AppUser appUser = userRepository.findUserById(id);
+    public void changePassword(@RequestParam("newpassword") String newPassword, @RequestParam("oldpassword") String oldPassword) throws InvalidOldPasswordException {
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //getting user from session
+        userDetails.getUsername();
+        AppUser appUser = userRepository.findUserByEmail(userDetails.getUsername());
         if(!passwordEncoder.matches(oldPassword, appUser.getPassword())) {
             throw new InvalidOldPasswordException();
         }
@@ -82,9 +89,14 @@ public class UserController {
     }
 
 
-    @GetMapping(value = "/{id}")
-    public Optional<AppUser> getUser(@PathVariable String id) {
-        return userRepository.findById(id);
+    @GetMapping(value = "/profile")
+    public AppUser getUser() {
+
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();  //getting user from session
+        userDetails.getUsername();
+        return userRepository.findUserByEmail(userDetails.getUsername());
+
     }
 
 
