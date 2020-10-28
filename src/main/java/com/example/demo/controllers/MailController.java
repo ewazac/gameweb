7cb0c0369917dd71b94b8fb278c657fb47c1e598
Gameweb <ewazac1@st.amu.edu.pl>
2020-10-28 23:14:21 +0100

    newsletter

--- src/main/java/com/example/demo/controllers/MailController.java
+++ src/main/java/com/example/demo/controllers/MailController.java
@@ -1,18 +1,30 @@
 package com.example.demo.controllers;
 
 import com.example.demo.model.AppUser;
+
 import com.example.demo.model.UserRepository;
 import com.example.demo.services.MailService;
+import com.mongodb.BasicDBObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.mail.MailException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static java.lang.Boolean.TRUE;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @RestController
+@CrossOrigin(origins = {"https://gameweb2.herokuapp.com","https://localhost:4200"}, allowCredentials = "true")
 public class MailController {
 
     private final MailService mailService;
@@ -24,20 +36,37 @@ public class MailController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping(value = "/newsletter")
-    public String sendMail() throws MailException {
-        try {
-            mailService.sendEmail();
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Autowired
+    MongoOperations mongoOperations;
+
+    @GetMapping()
+    public List<String> getAllCategories() {
+        Aggregation aggregation = newAggregation(group("category"), project("category"));
+        List<String> single = mongoOperations.aggregate(aggregation, "games", BasicDBObject.class).getMappedResults().stream().map(item -> item.getString("_id")).collect(Collectors.toList());
+        return single;
+    }
+
+    @GetMapping(value = "/newsletter")
+    public String sendMail(@RequestBody List<String> categories) throws MailException {
+        try {
+            if(categories != null) {
+                UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                AppUser user = userRepository.findUserByEmail(userDetails.getUsername());
+                user.setCategories(categories);
+                userRepository.save(user);
+                mailService.sendEmail();
+
+            }
         } catch (MailException mailException) {
             System.out.println(mailException);
         }
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        AppUser user = userRepository.findUserByEmail(userDetails.getUsername());
-        user.setNewsletter(TRUE);
-        userRepository.save(user);
-        return "Zapisano do newslettera.";
 
+        return "Zapisano do newslettera.";
     }
+
 
 }
