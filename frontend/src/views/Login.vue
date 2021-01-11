11978d9c8541aa60d80bde783fddef18e39434cf
Maggieses <magdalenadziasek@wp.pl>
2021-01-11 17:00:03 +0100

    login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,25 +5,29 @@
         <h1 class="login__header" style="margin-bottom:1rem;">Logowanie</h1>
         <div class="form-group">
           <b-form-input
-            required
-            v-model="user.username"
-            name="username"
-            type="text"
-            placeholder="Username"
+                  required
+                  v-model="user.username"
+                  name="username"
+                  type="text"
+                  placeholder="Username"
           />
         </div>
-        <div class="form-group">
+        <div class="password-group form-group">
           <b-form-input
-            required
-            v-model="user.password"
-            name="password"
-            type="password"
-            placeholder="Password"
-            autocomplete="on"
+                  required
+                  v-model="user.password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  autocomplete="on"
+                  id="password-input-id"
           />
+          <b-icon v-on:click="showPassword" id="icon-show-password-id" class="show-password current-icon" icon="eye"></b-icon>
+          <b-icon v-on:click="showPassword" id="icon-hide-password-id" class="show-password" icon="eye-slash-fill"></b-icon>
         </div>
         <p class="error" v-if="errorMessage != ''">{{ errorMessage }}</p>
         <hr />
+        <b-link class="login__link" href="#">Zapomniałeś hasła?</b-link>
         <b-link class="login__link" href="/Register">Nie masz konta? Zarejestruj się!</b-link>
         <hr />
         <div>
@@ -35,176 +39,212 @@
 </template>
 
 <script>
-import User from "../models/user";
+  import User from "../models/user";
 
-export default {
-  name: "Login",
-  data() {
-    return {
-      user: new User("", ""),
-      loading: false,
-      message: "",
-      errorMessage: "",
-    };
-  },
-  computed: {
-    loggedIn() {
-      return this.$store.state.auth.status.loggedIn;
+  export default {
+    name: "Login",
+    data() {
+      return {
+        user: new User("", ""),
+        loading: false,
+        message: "",
+        errorMessage: "",
+      };
     },
-  },
-  created() {
-    if (this.loggedIn) {
-      this.$router.push("/account");
-    }
-  },
-  beforeCreate: function () {
-    document.body.className = "app__body-login";
-  },
-  methods: {
-    handleLogin() {
-      this.loading = true;
-      this.$validator.validateAll().then((isValid) => {
-        if (!isValid) {
-          this.loading = false;
-          return;
-        }
-        if (this.user.username && this.user.password) {
-          this.$store.dispatch("auth/login", this.user).then(
-            () => {
-              this.$router.push("/account");
-            },
-            (error) => {
-              this.errorMessage = "Niepoprawne hasło lub login";
-              this.loading = false;
-              this.message =
-                (error.response && error.response.data) ||
-                error.message ||
-                error.toString();
-            }
-          );
-        }
-      });
+    computed: {
+      loggedIn() {
+        return this.$store.state.auth.status.loggedIn;
+      },
     },
-  },
-};
+    created() {
+      if (this.loggedIn) {
+        this.$router.push("/account");
+      }
+    },
+    beforeCreate: function () {
+      document.body.className = "app__body-login";
+    },
+    methods: {
+      handleLogin() {
+        this.loading = true;
+        this.$validator.validateAll().then((isValid) => {
+          if (!isValid) {
+            this.loading = false;
+            return;
+          }
+          if (this.user.username && this.user.password) {
+            this.$store.dispatch("auth/login", this.user).then(
+                    () => {
+                      this.$router.push("/account");
+                    },
+                    (error) => {
+                      this.errorMessage = "Niepoprawne hasło lub login";
+                      this.loading = false;
+                      this.message =
+                              (error.response && error.response.data) ||
+                              error.message ||
+                              error.toString();
+                    }
+            );
+          }
+        });
+      },
+      showPassword() {
+        var passwordInput = document.getElementById('password-input-id');
+        var iconShowPassword = document.getElementById("icon-show-password-id");
+        var iconHidePassword = document.getElementById("icon-hide-password-id");
+
+        if (passwordInput.type === "password") {
+          passwordInput.type = "text";
+          iconShowPassword.classList.remove("current-icon");
+          iconHidePassword.classList.add("current-icon");
+        } else {
+          passwordInput.type = "password";
+          iconHidePassword.classList.remove("current-icon");
+          iconShowPassword.classList.add("current-icon");
+        }
+      }
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.log {
-  display: block;
-  margin-top: 10px;
-}
-
-.login {
-  width: 50%;
-  background-color: rgba(247, 247, 247, 0.9);
-  padding: 20px 25px 30px;
-  margin: 0 auto 25px;
-  margin-top: 50px;
-  -moz-border-radius: 10px;
-  -webkit-border-radius: 10px;
-  border-radius: 10px;
-  transition: 0.2s;
-
-  &:hover {
-    -moz-box-shadow: 2px 2px 13px rgba(255, 255, 255, 0.5);
-    -webkit-box-shadow: 2px 2px 13px rgba(255, 255, 255, 0.5);
-    box-shadow: 2px 2px 13px rgba(255, 255, 255, 0.5);
-  }
-}
-
-.login__button {
-  background-color: #fa0b0b;
-  width: 100%;
-  border: none;
-  border-radius: 0;
-  padding: 10px 20px;
-  text-transform: uppercase;
-  font-weight: bold;
-  letter-spacing: 1px;
-  transition: 0.2s;
-
-  &:active {
-    background-color: #fa0b0b !important;
-    outline: none !important;
-    border: none !important;
-    box-shadow: none;
+  .log {
+    display: block;
+    margin-top: 10px;
   }
 
-  &:focus {
-    background-color: #fa0b0b!important;
-    outline: none !important;
-    border: none !important;
-    box-shadow: none !important;
-  }
-
-  &:hover {
-    background-color: #fa0b0b;
-    opacity: 0.9;
-  }
-}
-
-.login__link {
-  color: #fa0b0b;
-  text-decoration: underline;
-  display: inline-block;
-  width: 100%;
-  text-align: center;
-}
-
-.login__header {
-  margin-bottom: 1rem;
-  color: #fa0b0b;
-  text-transform: uppercase;
-  font-size: 30px;
-  font-weight: bold;
-}
-
-.form-group {
-  margin-top: 10px;
-  width: 100%;
-  display: flex;
-}
-
-input {
-  width: 100%;
-  border-width: 0px 0px 2px 0px;
-  border-radius: 4px;
-  border: 1px solid #111;
-  color: #fa0b0b;
-
-  &:active {
-    outline: none;
-    box-shadow: none;
-    border: 1px solid #111;
-    color: #fa0b0b;
-  }
-
-  &:focus {
-    outline: none;
-    box-shadow: none;
-    border: 1px solid #111;
-    color: #fa0b0b;
-  }
-}
-
-.error {
-  color: red;
-}
-
-h1 {
-  text-align: center;
-}
-
-@media (max-width: 991px) {
   .login {
-    width: 75%;
+    width: 50%;
+    background-color: #222;
+    padding: 20px 25px 30px;
+    margin: 0 auto 25px;
+    margin-top: 50px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+    transition: 0.2s;
+    border-radius: 0;
   }
-}
 
-@media (max-width: 767px) {
-  .login {
+  .login__button {
+    background-color: mediumaquamarine;
     width: 100%;
+    border: none;
+    border-radius: 0;
+    padding: 10px 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 1px;
+    transition: 0.2s;
+
+    &:active {
+      background-color: #66cdaa;
+      filter: brightness(1.1);
+      outline: none !important;
+      border: none !important;
+      box-shadow: none;
+    }
+
+    &:focus {
+      background-color: #66cdaa;
+      filter: brightness(1.1);
+      outline: none !important;
+      border: none !important;
+      box-shadow: none !important;
+    }
+
+    &:hover {
+      background-color: mediumaquamarine;
+      filter: brightness(1.1);
+    }
   }
-}
+
+  .form-control {
+    background-color: #000;
+  }
+
+  .login__link {
+    color: gray;
+    text-decoration: underline;
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+  }
+
+  .login__header {
+    margin-bottom: 1rem;
+    color: mediumaquamarine;
+    text-transform: uppercase;
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  .form-group {
+    margin-top: 10px;
+    width: 100%;
+    display: flex;
+  }
+
+  .password-group {
+    position: relative;
+  }
+
+  .show-password {
+    position: absolute;
+    color: darkgrey;
+    font-size: 26px;
+    top: 6px;
+    right: 8px;
+    display: none;
+    cursor: pointer;
+  }
+
+  .current-icon {
+    display: inline !important;
+  }
+
+  input {
+    width: 100%;
+    border-width: 0px 0px 2px 0px;
+    border-radius: 4px;
+    border: 1px solid #111;
+    color: mediumaquamarine;
+    background-color: #000;
+
+    &:active {
+      outline: none;
+      box-shadow: none;
+      border: 1px solid #111;
+      color: mediumaquamarine;
+      background-color: #000;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+      border: 1px solid #111;
+      color: mediumaquamarine;
+      background-color: #000;
+    }
+  }
+
+  .error {
+    color: red;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  @media (max-width: 991px) {
+    .login {
+      width: 75%;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .login {
+      width: 100%;
+    }
+  }
 </style>
