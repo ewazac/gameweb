24742c3d92466aea93b2b0cb7755f95ccb100304
Maggieses <magdalenadziasek@wp.pl>
2021-02-13 12:22:33 +0100

    obsługa błędów + resetPassword

---
+++ frontend/src/views/messages/messages.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="messages">
+        <div>
+            <b-alert :key="index" show :variant="message.type" v-for="(message, index) in messages">{{message.text}}</b-alert>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data:() => {
+            return{
+                test: '',
+                test_2: ''
+            }
+        },
+        computed:{
+            messages(){
+                return this.$store.getters['app/messages'];
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    .messages{
+        z-index: 999999;
+        position: fixed;
+        bottom: 10vh;
+        width: 50%;
+        left: 25%;
+    }
+</style>
---
+++ frontend/src/views/users/resetPassword.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="container log">
+        <form class="login" @submit.prevent="handleReset()">
+            <div class="form">
+                <h1 class="login__header" style="margin-bottom:1rem;"><span v-if="!token">Resetuj hasło</span><span v-else>Wprowadź nowe hasło</span></h1>
+                <div class="form-group" v-if="!token">
+                    <b-form-input
+                            required
+                            v-model="user.username"
+                            name="username"
+                            type="email"
+                            placeholder="Podaj adres e-mail"
+                    />
+                </div>
+                <div class="form-group" v-else>
+                    <b-form-input
+                            v-model="user.password"
+                            v-validate="'required|min:4|max:40'"
+                            type="password"
+                            class="form-control"
+                            name="password"
+                            placeholder="Nowe hasło"
+                            autocomplete="on"
+                    />
+                    <div
+                            v-if="submitted && errors.has('password')"
+                            class="alert-danger"
+                    >{{ 'Hasło musi zawierać co najmniej 4 znaki' }}</div>
+                </div>
+                <b-link class="login__link" href="/Register">Nie masz konta? Zarejestruj się!</b-link>
+                <hr />
+                <div>
+                    <b-button class="login__button" type="submit"><span v-if="token">Resetuj hasło</span><span v-else>Wyślij link resetowania hasła</span></b-button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import User from "../../models/user";
+
+    export default {
+        name: "Login",
+        data() {
+            return {
+                user: new User("", ""),
+                loading: false,
+                message: "",
+                errorMessage: "",
+                token: null
+            };
+        },
+        computed: {
+            loggedIn() {
+                return this.$store.state.auth.status.loggedIn;
+            },
+        },
+        created() {
+            if (this.loggedIn) {
+                this.$router.push("/account");
+            }
+            this.token = this.$route.query.token;
+        },
+        beforeCreate: function () {
+            document.body.className = "app__body-login";
+        },
+        methods: {
+            handleReset(){
+                this.loading = true;
+                this.$validator.validateAll().then((isValid) => {
+                    if (!isValid) {
+                        this.loading = false;
+                        return;
+                    }
+                    this.$store.dispatch()
+                });
+            },
+            handleLogin() {
+                this.loading = true;
+                this.$validator.validateAll().then((isValid) => {
+                    if (!isValid) {
+                        this.loading = false;
+                        return;
+                    }
+                    if (this.user.username && this.user.password) {
+                        this.$store.dispatch("auth/login", this.user).then(
+                            () => {
+                                this.$router.push("/account");
+                            },
+                            (error) => {
+                                this.errorMessage = "Niepoprawne hasło lub login";
+                                this.loading = false;
+                                this.message =
+                                    (error.response && error.response.data) ||
+                                    error.message ||
+                                    error.toString();
+                            }
+                        );
+                    }
+                });
+            },
+            showPassword() {
+                var passwordInput = document.getElementById('password-input-id');
+                var iconShowPassword = document.getElementById("icon-show-password-id");
+                var iconHidePassword = document.getElementById("icon-hide-password-id");
+
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                    iconShowPassword.classList.remove("current-icon");
+                    iconHidePassword.classList.add("current-icon");
+                } else {
+                    passwordInput.type = "password";
+                    iconHidePassword.classList.remove("current-icon");
+                    iconShowPassword.classList.add("current-icon");
+                }
+            }
+        },
+    };
+</script>
