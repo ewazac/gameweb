17aa4b39f96b9b91a5cb3fd81e1a7c4bcca82ccc
Gameweb <ewazac1@st.amu.edu.pl>
2021-03-08 09:17:32 +0100

    newsletter

--- src/main/java/com/example/demo/scheduler/NewsletterScheduler.java
+++ src/main/java/com/example/demo/scheduler/NewsletterScheduler.java
@@ -1,6 +1,6 @@
 package com.example.demo.scheduler;
 
-import com.example.demo.model.dao.Game;
+
 import com.example.demo.model.dao.News;
 import com.example.demo.model.dao.Quiz;
 import com.example.demo.repository.GamesRepository;
@@ -26,16 +26,17 @@ public class NewsletterScheduler {
     private final QuizRepository quizRepository;
     private final GamesRepository gamesRepository;
 
-    @Scheduled(cron = "0 0 7 ? * MON")
+    //@Scheduled(cron = "0 0 7 ? * MON")
+    @Scheduled(cron = "0 45,50 9 ? * MON")
 //    @Scheduled(fixedRate = 10000)
     public void sendNewsletter() {
         List<News> news = newsRepository.findByCreatedDateIsAfter(LocalDate.now().minusDays(7));
         List<Quiz> quiz = quizRepository.findByCreatedDateIsAfter(LocalDate.now().minusDays(7));
-        List<Game> competition = gamesRepository.findByCreatedDateIsAfter(LocalDate.now().minusDays(7));
+//        List<Game> competition = gamesRepository.findByCreatedDateIsAfter(LocalDate.now().minusDays(7));
         Context context = new Context();
         context.setVariable("news", news);
         context.setVariable("quiz", quiz);
-        context.setVariable("competition", competition);
+        //context.setVariable("competition", competition);
         userRepository.findByNewsletterIsTrue().forEach(user -> {
             mailService.sendMail("newsletter", user.getEmail(), context);
         });
