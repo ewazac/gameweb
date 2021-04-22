90dfbd49b1718fe5abbaff07d79e08f0b594ad30
Thrytios <dembiski321@gmail.com>
2021-04-22 18:00:25 +0200

    Poprawa App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,10 +5,10 @@
             <b-navbar-toggle class="app__toggle-menu ml-auto" target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="mr-auto align-items-baseline">
-                    <b-nav-item href="/Games">
+                    <b-nav-item to="/Games">
                         <img class="GameWeb" src="./assets/logo.png" alt="GameWeb" />
                     </b-nav-item>
-                    <b-nav-item href="/News">
+                    <b-nav-item to="/News">
                         News
                     </b-nav-item>
                     <router-link to="/quizy">
@@ -27,23 +27,23 @@
                     </b-nav-item>
                 </b-navbar-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/favourites" id="fav" v-if="currentLoggedIn">
+                    <b-nav-item to="/favourites" id="fav" v-if="currentLoggedIn">
                         <div style="position: relative">
                             <b-icon icon="suit-heart-fill" aria-hidden="fav"></b-icon>
                             <div style="position: absolute; top: -8px; right: -8px; padding: 3px 7px; font-size: 11px; border-radius: 100%; background-color: red">{{favourites.length}}</div>
                         </div>
 
                     </b-nav-item>
-                    <b-nav-item href="/search" id="search-icon-id">
+                    <b-nav-item to="/search" id="search-icon-id">
                         <b-icon icon="search" aria-hidden="search"></b-icon>
                     </b-nav-item>
-                    <b-nav-item v-if="currentLoggedIn === false" href="/login">
+                    <b-nav-item v-if="currentLoggedIn === false" to="/login">
                         <b-icon icon="person" aria-hidden="login"></b-icon>
                     </b-nav-item>
-                    <b-nav-item v-if="currentLoggedIn === true" href="/account">
+                    <b-nav-item v-if="currentLoggedIn === true" to="/account">
                         <b-icon icon="gear-fill" aria-hidden="settings"></b-icon>
                     </b-nav-item>
-                    <b-nav-item v-if="currentLoggedIn === true" href="javascript:void(0);">
+                    <b-nav-item v-if="currentLoggedIn === true" to="javascript:void(0);">
                         <router-link to="/" @click.native.prevent="logOut" class="nav-link logout">
                             <b-icon icon="power" aria-hidden="true" class="logout"></b-icon>
                         </router-link>
